43f3fc866e8f50eac108edc046da6419ed03cedc
Pedro Joaquim Freire de Lima <pedro.joaquim@estudante.ifro.edu.br>
2023-07-26 20:10:04 -0400

    first commit

---
+++ FormNasci/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormNasci
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime data = datenasc.Value;
+
+            if (data < DateTime.Now)
+            {
+                int idade = DateTime.Now.Year - data.Year;
+
+                if (DateTime.Now.Month < data.Month || (DateTime.Now.Month == data.Month && DateTime.Now.Day < data.Day))
+                {
+                    idade = idade - 1;
+                    label1.Text = idade.ToString();
+                }
+                else
+                {
+                    label1.Text = idade.ToString();
+                }
+            }
+            else
+            {
+                label1.Text = "Idade fornecida incorreta!";
+            }
+        }
+    }
+}
